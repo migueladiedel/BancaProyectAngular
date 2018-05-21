eec9f84114d35bc077eb1a3aa59bf5472e6446af
Formación <maddgm92@gmail.com>
2018-05-21 15:50:17 +0200

    Inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PageNotFoundComponentComponent } from './page-not-found-component/page-not-found-component.component';
+import { MenuComponent } from './menu/menu.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { BlogComponent } from './blog/blog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageNotFoundComponentComponent,
+    MenuComponent,
+    LoginComponent,
+    HomeComponent,
+    BlogComponent
   ],
   imports: [
     BrowserModule
