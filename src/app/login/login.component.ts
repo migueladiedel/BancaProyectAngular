404c949a9b8bf05f4aed7fe0b9a48383fb788e21
migueladiedel <maddgm92@gmail.com>
2018-05-21 23:42:10 +0200

    Login funcionando

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from '../services/seguridad.service';
+import { NotifyService } from '../services/notify.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  txtButon = 'Log In';
+  txtUsuario = 'admin';
+  txtPassword = 'P@$$w0rd';
 
-  constructor() { }
+  constructor(public loginSrv: LoginService, private nsrv: NotifyService, private router: Router) { }
 
   ngOnInit() {
+    this.cambiaTexto();
   }
 
+  logInOut() {
+    if (this.loginSrv.isAutenticated) {
+      this.loginSrv.logout();
+      this.cambiaTexto();
+    } else {
+      this.loginSrv.login(this.txtUsuario, this.txtPassword).subscribe(
+        data => {
+          if (data) {
+            this.cambiaTexto();
+          } else {
+            this.nsrv.add('Usuario o contraseña invalida.');
+          }
+        },
+        err => { this.nsrv.add(err.message); }
+      );
+    }
+  }
+
+  registrar() {
+    this.router.navigateByUrl('/registro');
+  }
+
+  private cambiaTexto() {
+    this.txtButon = this.loginSrv.isAutenticated ? 'Log Out' : 'Log In';
+  }
 }
